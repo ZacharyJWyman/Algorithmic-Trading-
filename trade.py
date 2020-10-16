be69889e5860dbf96dc1e9587a205843ca216319
Zachary Wyman <zack.wyman1@#gmail.com>
2020-10-16 14:20:15 -0700

    integrate dict

--- trade.py
+++ trade.py
@@ -2,37 +2,47 @@ from config import *
 from main_script import *
 import yfinance as yf
 import time
+import schedule
+from collections import defaultdict
 
 #stocks to watch
 tickers = ['MSFT', 'AAPL', 'AMZN', 'FB', 'GOOG']
+
+#CONSTS
 CLOCK = clock()
 MARKET_OPEN = CLOCK['next_open'] #need to specify script to trade between these times
 MARKET_CLOSE = CLOCK['next_close']
+PRICE = 0
+stock_dict = defaultdict(list)
+
 
 #below line will integrate clock into script. remove # when ready.
-#if CLOCK['is_open'] is True: #specifies if market is open
-#else: 
-    #print('market is open at: ' + str(CLOCK['next_open']))
-while True:
-
-#order = create_order('MSFT', 1, 'buy', 'market', 'day')
-
-    #request price every minute on MSFT (2000 requests per hour = 33 requests per min)
-    MSFT = yf.Ticker('MSFT')
-    print('Ask Price:' + str(MSFT.info['ask']))
-    print('NEW REQUEST')
-    time.sleep(3)
-
+while CLOCK['is_open'] is False: #specifies if market is open
     
+    #code for first time append 0 to stock price
+        
+
+    for tick in tickers:
+        #request price every minute on MSFT (2000 requests per hour = 33 requests per min)
+        obj = yf.Ticker(tick)
+        price = obj.info['ask']
+        stock = obj.info['symbol']
+        print('Ask Price:' + str(price))
+        time.sleep(1)
 
 
+        if price > stock_dict[stock][-1]: #if next ping greater than prior ping
+            order = create_order(tick, 1, 'buy', 'market', 'day')
+            print('Trade Confirmed')
+            time.sleep(1)
+        else:
+            print('Price Decreased: No trade made')
 
-    #makes trade every 10 seconds until script is canceled #ctrl c
-    order = create_order('MSFT', 1, 'buy', 'market', 'day')
-    time.sleep(10)
-    print('Trade Confirmed')
-    
-
+        #append price to end of dict
+        stock_dict[stock].append(price)
+        
+else: 
+    print('market is open at: ' + str(CLOCK['next_open']))
     
     
 
